a1b4afe92cc78e13391f6ad3cea8bda9ab8fd849
AlekseyBelyaev2606 <partchoice44@gmail.com>
2023-01-27 14:58:54 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int second = int.Parse(Console.ReadLine());
+if (first > second){
+    Console.WriteLine($"Первое число {first} больше второго числа {second}");
+}
+else if (first == second){
+    Console.WriteLine($"Первое число {first} равно числу {second}");
+}
+else{
+    Console.WriteLine($"Первое число {first} меньше второго числа {second}");
+}
